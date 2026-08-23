f53c58bf6f1a762ec559bda949b2b79bcb5f7f04
Ashwin Bhat K <ashwinbhatk00@gmail.com>
2026-08-23 21:07:05 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(auto &s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> res;
+
+        for(auto &[key, grp] : mp) {
+            res.push_back(grp);
+        }
+        return res;
+    }
+};
